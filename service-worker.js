7af8b04213e0e10c21b83d6ee004024eb2d0fcb3
brinahfah <ebikie4@gmail.com>
2025-12-23 23:47:23 +0100

    main

--- service-worker.js
+++ service-worker.js
@@ -1,69 +1,33 @@
-const CACHE_NAME = 'todolist-cache-v4';
+const CACHE_NAME = 'todolist-cache-v5';
+
 const urlsToCache = [
-    '/Todolist_pwa/',
-    '/Todolist_pwa/index.html',
-    '/Todolist_pwa/style.css',
-    '/Todolist_pwa/script.js',
-    '/Todolist_pwa/manifest.json',
-    '/Todolist_pwa/images/icon-192x192.png',
-    '/Todolist_pwa/icon-512x512.png'
+  '/Todolist_pwa/',
+  '/Todolist_pwa/index.html',
+  '/Todolist_pwa/style.css',
+  '/Todolist_pwa/script.js',
+  '/Todolist_pwa/manifest.json',
+  '/Todolist_pwa/images/icon-192x192.png',
+  '/Todolist_pwa/images/icon-512x512.png'
 ];
 
-// Événement d'installation : mise en cache des ressources essentielles
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Mise en cache des ressources.');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-// Événement de récupération : interception des requêtes et tentative de réponse depuis le cache
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Si la ressource est en cache, on la renvoie
-                if (response) {
-                    return response;
-                }
-                // Sinon, on la récupère depuis le réseau
-                return fetch(event.request).then(
-                    response => {
-                        // On vérifie si la réponse est valide
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // On clone la réponse pour pouvoir la mettre en cache
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
-            })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
 
-// Événement d'activation : gestion des anciennes versions du cache
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
